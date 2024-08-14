d2e5d92277687a324391565cfdd6934cf16bfbd6
RumasorengFadil <limitvariabel@gmail.com>
2024-08-14 13:45:00 +0700

    create mstPlace model, migration, and seeder

--- app/Models/MstPublisher.php
+++ app/Models/MstPublisher.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class MstPublisher extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'publisher_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "publisher_name",
+        "input_date",
+        "last_update",
+    ];
+
+    public static function addMember($request)
+    {
+        Member::create([
+            "publisher_name" => $request->publisherName,
+            "input_date" => now()->toDateString(),
+            "last_update" => now()->toDateString(),
+        ]);
+    }
 }
