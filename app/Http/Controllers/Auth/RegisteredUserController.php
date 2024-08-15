1dd075b92e777d18601f3ae95960785b6b70da4b
Rumasoreng Fadil <limitvariabel@gmail.com>
2024-08-15 08:42:32 +0700

    Created Item model, migration, controller, seeder, and factory

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
         //     'password' => ['required', 'confirmed', Rules\Password::defaults()],
         // ]);
 
-        $user = User::register($request);
+        $user = User::register();
         
         event(new Registered($user));
 
