cc56741a7b0501a8ae4405c05287708356ccc7d0
Rumasoreng Fadil <limitvariabel@gmail.com>
2024-08-27 14:56:09 +0700

    feat (Layouts) : Created top bar layout

--- resources/js/Layouts/SidebarLayout.jsx
+++ resources/js/Layouts/SidebarLayout.jsx
@@ -1,7 +1,7 @@
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import { Link } from "@inertiajs/react";
 
-export default function NavbarLayout({ children }) {
+export default function SidebarLayout({ children }) {
     return (
         <div className="bg-light-gray min-h-screen">
             <nav className="w-72 bg-primary min-h-screen">
@@ -9,9 +9,10 @@ export default function NavbarLayout({ children }) {
                     <img className="w-24 h-24 mr-6 bg-white p-1 rounded-full" src="/img/users/user-profile-picture.png"></img>
                     <p className="text-xl font-inter font-bold text-center">Admin <br /> <span className="font-normal">Jonas</span></p>
                 </div>
-                <div className="flex bg-black p-10 items-center justify-center">
+                <div className="border-y border-opacity-20 border-black pt-6 pb-6">
                     <ul>
-                        <li>Daftar anggota</li>
+                       {children}
+                        
                     </ul>
                 </div>
             </nav>
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,9 +1,30 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import NavbarLayout from "@/Layouts/NavbarLayout";
+import SidebarLayout from "@/Layouts/SidebarLayout";
 import { Head } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
-    return <NavbarLayout>
-        
-    </NavbarLayout>;
+    return (
+        <div className="flex">
+            <SidebarLayout>
+                <a href="">
+                    <li className="bg-white mb-1 shadow-lg text-xl cursor-pointer w-64 p-2 rounded-r-full font-semibold text-primary">
+                        Tambah anggota
+                    </li>
+                </a>
+                <a href="">
+                    <li className="text-xl mb-1 cursor-pointer w-64 p-2 rounded-r-full font-semibold text-white">
+                    Daftar anggota
+                    </li>
+                </a>
+                <a href="">
+                    <li className="mb-1 text-xl cursor-pointer w-64 p-2 rounded-r-full font-semibold text-white">
+                        Non-aktifkan anggota
+                    </li>
+                </a>
+            </SidebarLayout>
+
+            
+        </div>
+    );
 }
