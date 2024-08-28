726e1f67825480106eb6c1013e0215990a465985
Rumasoreng Fadil <limitvariabel@gmail.com>
2024-08-28 21:07:58 +0700

    feat (Pages) : Created Circulation Page

---
+++ resources/js/Pages/Circulation/Circulation.jsx
@@ -0,0 +1,23 @@
+import BibliographySidebar from "@/Components/Sidebar/BibliographySidebar";
+import MainContentLayout from "@/Layouts/MainContentLayout";
+import SidebarLayout from "@/Layouts/SidebarLayout";
+import TopbarLayout from "@/Layouts/TopbarLayout";
+import { Head, Link, usePage } from "@inertiajs/react";
+
+export default function Circulation({}) {
+    return (
+        <div className="flex fixed min-w-full bg-light-gray min-h-screen">
+            <SidebarLayout>
+                <BibliographySidebar />
+            </SidebarLayout>
+
+            <div className="flex flex-col w-full">
+                <TopbarLayout />
+
+                <MainContentLayout>
+                    <h1 className="text-center mt-32">Daftar Pustaka</h1>
+                </MainContentLayout>
+            </div>
+        </div>
+    );
+}
