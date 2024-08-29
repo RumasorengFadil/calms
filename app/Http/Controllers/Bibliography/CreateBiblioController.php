c707e6938541db869a762e4fff03945c448243e3
Rumasoreng Fadil <limitvariabel@gmail.com>
2024-08-29 09:21:25 +0700

    chore (Controllers) : Rename AddBiblioController to CreateBiblioController

--- app/Http/Controllers/Bibliography/CreateBiblioController.php
+++ app/Http/Controllers/Bibliography/CreateBiblioController.php
@@ -7,14 +7,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Bibliography\AddBiblioRequest;
 use App\Services\BiblioService;
 
-class AddBiblioController extends Controller
+class CreateBiblioController extends Controller
 {
     protected $biblioService;
     public function __construct(BiblioService $biblioService)
     {
         $this->biblioService = $biblioService;
     }
-    public function addBiblio(AddBiblioRequest $request)
+    public function createBiblio(AddBiblioRequest $request)
     {
         try {
             // Data sudah tervalidasi oleh AddBiblioRequest
