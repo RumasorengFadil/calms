4cce0477b5a7e9de186163b89de2784ec2124792
Rumasoreng Fadil <limitvariabel@gmail.com>
2024-09-08 11:35:40 +0700

    close #3 : fixed create are returning blank page

--- resources/js/Pages/Bibliography/CreateBiblioGraphy.jsx
+++ resources/js/Pages/Bibliography/CreateBiblioGraphy.jsx
@@ -4,6 +4,8 @@ import SidebarLayout from "@/Layouts/SidebarLayout";
 import TopbarLayout from "@/Layouts/TopbarLayout";
 import { Head, Link, usePage } from "@inertiajs/react";
 import PageHeader from "@/Components/PageHeader";
+import TextInput from "@/Components/TextInput";
+import PrimaryButton from "@/Components/PrimaryButton";
 
 export default function CreateBibliography({}) {
     return (
@@ -17,6 +19,32 @@ export default function CreateBibliography({}) {
 
                 <MainContentLayout>
                     <PageHeader>Tambah Pustaka</PageHeader>
+
+                    <div className="bg-white px-10">
+                        <form action="">
+                            <div className="flex items-center py-3">
+                                <label htmlFor="title" className="basis-80">Judul</label>
+                                <span className="mx-7">:</span>
+                                <TextInput type="text" className="p-1 basis-full"  />
+                            </div>
+                            <div className="flex items-center py-3">
+                                <label htmlFor="publisher" className="basis-80">Publisher</label>
+                                <span className="mx-7">:</span>
+                                <TextInput type="text" className="p-1 basis-full"  />
+                            </div>
+                            <div className="flex items-center py-3">
+                                <label htmlFor="author" className="basis-80">Author</label>
+                                <span className="mx-7">:</span>
+
+                                <div className="flex flex-col basis-full">
+                                    <PrimaryButton className="bg-gray-400 w-max">Tambah Penulis</PrimaryButton>
+                                    <div className="border h-full min-h-40">
+                                        {/* <div>dsdsd</div> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </MainContentLayout>
             </div>
         </div>
